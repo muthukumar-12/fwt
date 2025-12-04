a19573b88e9d98dc89e7107a72a5e84b88153af0
muthuronaldo5-del <muthuronaldo5@gmail.com>
2025-12-04 00:38:58 -0800

    reaerch upadted

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
  import Footer from "./component/layout/Footer.jsx";  
  import {ExperienceSection} from "./component/pages/ExperienceSection.jsx";
  import {Projectssection} from "./component/pages/Projectssection.jsx"; 
- import {ResearchSection} from "./component/pages/ResearchSection.jsx";
 import {AwardsSection} from "./component/pages/AwardsSection.jsx";
 import {AchievementsGrid } from "./component/pages/AchievementsGrid.jsx";
 import {BooksGrid} from "./component/pages/BooksGrid.jsx";
 import { AppointmentsSection } from "./component/pages/AppointmentsSection.jsx";
- 
-
+ import {ResearchSection} from "./component/pages/ResearchSection.jsx";
  
  function App() {
  return (
